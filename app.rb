d5ca527fce3505cf55b4cb15e5ee063e257d63d0
Vasiliy R <ya-kubanec@yandex.ru>
2024-07-14 14:47:56 +0300

    Redirect to main page while adding a post

--- app.rb
+++ app.rb
@@ -57,5 +57,7 @@ post '/new' do
   # сохранение данных в БД
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
-  erb "You typed #{content}"
+  # перенаправление на главную страницу
+  redirect to '/'
+  
 end
