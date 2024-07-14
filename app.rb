a0e581ebae50f717d32cfcc758a999fdf5512ef6
Vasiliy R <ya-kubanec@yandex.ru>
2024-07-14 13:35:16 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -47,5 +47,10 @@ post '/new' do
   # получаем переменную из post-запроса
   content = params[:content]
 
+  if content.length <= 0
+  	@error = 'Type post text'
+  	return erb :new
+  end
+
   erb "You typed #{content}"
 end
